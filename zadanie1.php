fe465a6a8a04fe568cc21f8a5ef10d0c2abbd147
KiloGipsu <mateuszjas12398@gmail.com>
2021-03-31 13:10:37 +0200

    dodanie sprawdzenia

--- zadanie1.php
+++ zadanie1.php
@@ -6,5 +6,10 @@
 
     $con = mysqli_connect($host,$user,$passwd,$dbname);
 
+    if($con->connect_error){
+        echo("błąd".$con->connect_error);
+    }else{
+        echo('połączono');
+    }
 
 ?>
